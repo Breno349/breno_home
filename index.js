11c99eb143bdfc5b2b89c22da3ccb1d1e766469f
Breno349 <123127632+Breno349@users.noreply.github.com>
2023-01-27 16:38:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 txt.innerText = "Inicializado."
+try {
+  import makeWASocket from "./node_modules/@adiwajshing/baileys"
+  txt.innerText = "importado zequinha doida."
+} catch (err){
+  txt.innerText = "Erro: "+err;
+}
