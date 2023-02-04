461df167952584d79eb264b7993319b62bf4af9d
Breno349 <brenoserrayo10@gmail.com>
2023-02-04 17:10:29 -0300

    Removed all!

--- index.js
+++
@@ -1,7 +0,0 @@
-txt.innerText = "Inicializado."
-try {
-  import makeWASocket from "./node_modules/@adiwajshing/baileys"
-  txt.innerText = "importado zequinha doida."
-} catch (err){
-  txt.innerText = "Erro: "+err;
-}
